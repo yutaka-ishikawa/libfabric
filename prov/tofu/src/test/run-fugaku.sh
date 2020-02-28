72a0181642e53b35ec61dd5dd9550d6c7335fe3c
Yutaka Ishikawa <yutaka.ishikawa@riken.jp>
2020-02-28 09:27:02 +0900

    coordinate data

--- prov/tofu/src/test/run-fugaku.sh
+++ prov/tofu/src/test/run-fugaku.sh
@@ -7,14 +7,16 @@
 #	#PJM --mpi "proc=48"
 #	#PJM -L "node=64"
 #	#PJM --mpi "proc=64"
-#PJM -L "node=256"
-#PJM --mpi "proc=256"
+#	#PJM -L "node=192"
+#	#PJM --mpi "proc=192"
+#PJM -L "node=4608"
+#PJM --mpi "proc=4608"
 #PJM -L "elapse=00:01:00"
 #PJM -L "rscunit=rscunit_ft01,rscgrp=dv27k"
 #PJM -L proc-core=unlimited
 #------- Program execution -------#
 
 module load lang
-mpiexec -np 256 ./procmap
+mpiexec -np 4608 ./procmap
 #./size_exam
 #./size_exam2
