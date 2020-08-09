f4945b127f23335c4019fe5e351109554fc302c6
Yutaka Ishikawa <yutaka.ishikawa@riken.jp>
2020-08-09 20:01:36 +0900

    clean up this branch

--- prov/tofu/src/utf_engine.h
+++
@@ -1,9 +0,0 @@
-static inline void
-utf_reset_chain_info(struct utf_send_cntr *usp)
-{
-    usp->chn_ready.data = 0; 
-    usp->chn_next.rank = RANK_ALL1;
-    usp->evtupdt = 0;
-    usp->expevtupdt = 0;
-    usp->chn_informed = 0;
-}
